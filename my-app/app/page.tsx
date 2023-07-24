59f7b08f71576dbe7d43c06ca129423897b03c65
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-07-24 06:05:30 -0700

    init

---
+++ my-app/app/page.tsx
@@ -0,0 +1,14 @@
+
+"use client"
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+  <Button 
+  
+  >
+Hello
+  </Button>  
+  )
+}
